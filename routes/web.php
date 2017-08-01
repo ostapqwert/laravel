470280790ffbee9718cdb49e319cfdfcb72d90ec
Остап Бендер <ostapbender@MacBook.local>
2017-08-01 17:35:13 +0400

    del comments

--- routes/web.php
+++ routes/web.php
@@ -15,16 +15,7 @@
 //    return view('welcome');
 //});
 
-//Route::get('/test', function () {
-//    $user = \App\User::find(1);
-//
-//    $posts = $user->posts;
-//
-//    foreach($posts as $post){
-//        echo $post->id.'<br>';
-//        echo $post->body.'<br>';
-//    }
-//});
+
 
 
 Auth::routes();
